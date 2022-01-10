ac8e9f91bb44011816c6d3942e8f6c0f77517f04
Дмитрий Минаков <m1nak0vd@yandex.ru>
2022-01-10 19:46:30 +0300

    Добавьте файлы проекта.

---
+++ Lab_12/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cat.voice);
+            Console.ReadKey();
+        }
+        public class Cat
+        {
+            public static string voice;
+            public const int countFeet = 4;
+            public readonly bool wool = true;
+            public string Neme { get; set; }
+
+            public string Color { get; set; }
+
+            int age;
+            public int Age
+            {
+                set
+                {
+                    if (value > 0)
+                    {
+                        age = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Возраст не может быть отрицательным");
+                    }
+
+                }
+                get
+                {
+                    return age;
+                }
+            }
+            static Cat()
+            {
+                voice = "Мяу";
+            }
+            public Cat(string neme, string color, int age, bool wool)
+            {
+                Neme = neme;
+                Color = color;
+                Age = age;
+                this.wool = wool;
+            }
+            public void Say()
+            {
+                Console.WriteLine("Мяу! Меня зовут {0}! У меня {1} цвет. Мне {2} лет", Neme, Color, Age);
+            }
+        }
+    }
+}
